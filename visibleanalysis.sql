d0da5d961c52667b1f278d07d5c9adf5cc3c21d2
庚晨希未 <50995038+lxbzxzy@users.noreply.github.com>
2020-09-06 14:10:33 +0800

    Update visibleanalysis.sql

--- visibleanalysis.sql
+++ visibleanalysis.sql
@@ -6,11 +6,15 @@
 --注意点：
 --● 将日期拖入筛选器，以日为单位计算市值之和
 --● 地图中有部分城市/地域无法自动识别，需要手动输入经纬度
+--原始项目路径：visible analysis/历年沪深300行业和地域分布.twb
+--原始项目路径：visible analysis/历年沪深300行业和地域分布视频.mp4(github暂不支持在线查看视频功能)
 
 --2. 可视化任务一中得到的沪深300成分股总市值的地域和行业分布并制作动图展现分布情况随时间的变化。
 --合并两市股票时需要将所属行业字段统一，
 --将sh_stock_list中入选沪深300的股票的的行业并入sz_stock_list中的所属行业字段。
 --为制作动图效果，将日期字段拖入页面，同样以日为单位。
+--原始项目路径：visible analysis/深交所行业和地域分布.twb
+--原始项目路径：visible analysis/深交所行业和地域分布.mp4(github暂不支持在线查看视频功能)
 
 create table totalCSI300(原简称 varchar(32), 股票简称 varchar(32), 
 	股票代码 numeric(6,0) not null, 省份 varchar(32), 所属行业 varchar(32), 
